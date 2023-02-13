fac024553bea671fe9c3aee28d67bc6ed18e11e3
svADMIN <svyouth_admin@MacBook-Air-som-tillhor-svYOUTH.local>
2023-02-13 19:21:21 +0100

    done

---
+++ Maine.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author svyouth_admin
+ */
+public class Maine{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println (x:"Hej");
+        System.out.println (x:"Hej");
+    }
+}
